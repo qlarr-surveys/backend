cdb7b01a2edd153fc6380b4a12a9e407db408e46
Amr Askoura <askoura@qlarr..com>
2024-07-11 23:38:36 +0200

    remove actual files

--- frankie-lib/expressionmanager/src/main/java/com/frankie/expressionmanager/usecase/DesignerInput.kt
+++
@@ -1,9 +0,0 @@
-package com.frankie.expressionmanager.usecase
-
-import com.fasterxml.jackson.databind.node.ObjectNode
-import com.frankie.expressionmanager.model.ComponentIndex
-
-data class DesignerInput(
-    val state: ObjectNode,
-    val componentIndexList: List<ComponentIndex>
-)
